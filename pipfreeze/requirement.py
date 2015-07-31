e4a9154973e0c55586559c1479d9fea87082ec1f
Alex Conrad <alexandre@surveymonkey.com>
2015-07-31 18:48:24 +0000

    add Requirement.id

--- pipfreeze/requirement.py
+++ pipfreeze/requirement.py
@@ -6,5 +6,6 @@ class Requirement(object):
         self._req = pkg_resources.Requirement.parse(requirement)
 
         self.raw = requirement
+        self.id = self._req.key
         self.name = self._req.project_name
         self.specifiers = self._req.specs
--- tests/test_requirement.py
+++ tests/test_requirement.py
@@ -1,13 +1,14 @@
 import pytest
 
 
-@pytest.mark.parametrize("req, req_name, req_specs, req_raw", [
-    ('foobar==1.2', 'foobar', [('==', '1.2')], 'foobar==1.2'),
-    ('foo>=1.2,<2.0', 'foo', [('>=', '1.2'), ('<', '2.0')], 'foo>=1.2,<2.0'),
+@pytest.mark.parametrize("req, req_name, req_specs, req_raw, req_id", [
+    ('foobar==1.2', 'foobar', [('==', '1.2')], 'foobar==1.2', 'foobar'),
+    ('foo>=1.2,<2.0', 'foo', [('>=', '1.2'), ('<', '2.0')], 'foo>=1.2,<2.0', 'foo'),
 ])
-def test_requirement(req, req_name, req_specs, req_raw):
+def test_requirement(req, req_name, req_specs, req_raw, req_id):
     from pipfreeze import Requirement
     req = Requirement(req)
-    req.name == req_name
-    req.specifiers == req_specs
-    req.raw == req_raw
+    assert req.id == req_id
+    assert req.name == req_name
+    assert req.specifiers == req_specs
+    assert req.raw == req_raw
