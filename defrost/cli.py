af203eeadd1c0b5d64c388241997a2dfa03de5f1
Alex Conrad <alexandre@surveymonkey.com>
2015-08-05 04:53:15 +0000

    exit code 1 if deprecation severity is error

--- defrost/cli.py
+++ defrost/cli.py
@@ -1,4 +1,5 @@
 import click
+import sys
 import yaml
 
 from defrost import PipFreeze
@@ -11,6 +12,8 @@ def defrost(requirement_file, pip_freeze_file):
     reqs = yaml.load(requirement_file)
     pip_freeze = PipFreeze(pip_freeze_file.read())
     pip_freeze.load_requirements(reqs)
+
+    exit_code = 0
     for package in pip_freeze.deprecated:
         click.echo("{}: {!r} does not satisfy {!r}: {}".format(
             package.deprecation_severity,
@@ -18,3 +21,7 @@ def defrost(requirement_file, pip_freeze_file):
             package.deprecated_by,
             package.deprecation_reason,
         ))
+        if package.deprecation_severity == 'error':
+            exit_code = 1
+
+    sys.exit(exit_code)
